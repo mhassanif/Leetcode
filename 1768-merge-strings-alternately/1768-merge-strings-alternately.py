48e020e69a0089dea0a04e6e718e2902eb6d390e
Hassan Imran <mhassanimranf@gmail.com>
2024-08-25 17:22:35 +0500

    Time: 22 ms (9.91%), Space: 11.6 MB (58.61%) - LeetHub

--- 1768-merge-strings-alternately/1768-merge-strings-alternately.py
+++ 1768-merge-strings-alternately/1768-merge-strings-alternately.py
@@ -1,9 +1,23 @@
+# :type word1: str
+# :type word2: str
+# :rtype: str
+
 class Solution(object):
     def mergeAlternately(self, word1, word2):
+        result = ""
+        for i in range (max(len(word1), len(word2))):
+            if i < len(word1):
+                result += word1[i]
+            if i < len(word2):
+                result += word2[i]
+        return result
         
-        """
-        :type word1: str
-        :type word2: str
-        :rtype: str
-        """
+            
+
+
+
+
+
+
+        
         
