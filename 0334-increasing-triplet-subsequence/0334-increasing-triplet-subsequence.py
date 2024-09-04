ab3de2ec4384cafb3c7e579d173df0ea81b236ce
Hassan Imran <mhassanimranf@gmail.com>
2024-09-04 21:25:21 +0500

    Time: 817 ms (73.36%), Space: 26.3 MB (49.76%) - LeetHub

--- 0334-increasing-triplet-subsequence/0334-increasing-triplet-subsequence.py
+++ 0334-increasing-triplet-subsequence/0334-increasing-triplet-subsequence.py
@@ -1,22 +1,31 @@
-# # :type nums: List[int]
-# # :rtype: bool
+# :type nums: List[int]
+# :rtype: bool
 
-# class Solution(object):
-#     def increasingTriplet(self, nums):
-#         # initialize with infinity
-#         first = second = third = float('inf') 
-#         second_i = len(nums)-1
-#         for n in nums: 
-#             # when updating ensure first isnt updated to index greater than second
-#             if n <= first : 
-#                 first = n
-#             elif n <= second:
-#                 second = n
-#                 second_i = nums.index(second)
-#             elif n <= third:
-#                 third = n
-#                 print(first,second,third)
-#                 return True
-#         return False
+class Solution(object):
+    def increasingTriplet(self, nums):
+        first = second = float('inf')
+    
+        for num in nums:
+            if num <= first:
+                first = num  
+            elif num <= second:
+                # smaller than second but larger than first
+                second = num  
+            else:
+                # num larger than first & second
+                return True
+        return False
 
+    
 
+# ALTERNATE SOLUTION  : O(N^2)
+
+# def increasingTriplet(self, nums):
+#     n = len(nums)
+#     for i in range(1, n - 1):
+#         left_smaller = any(nums[j] < nums[i] for j in range(i))
+#         right_larger = any(nums[k] > nums[i] for k in range(i + 1, n))
+        
+#         if left_smaller and right_larger:
+#             return True
+#     return False
