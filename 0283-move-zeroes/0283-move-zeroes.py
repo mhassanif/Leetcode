932841b31e27a6c620814b92578da1da429077e8
Hassan Imran <mhassanimranf@gmail.com>
2024-09-05 00:21:42 +0500

    Time: 104 ms (97.18%), Space: 12.8 MB (88.68%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.py
+++ 0283-move-zeroes/0283-move-zeroes.py
@@ -1,11 +1,23 @@
-# :type nums: List[int]
-# :rtype: None Do not return anything, modify nums in-place instead.
+# # :type nums: List[int]
+# # :rtype: None Do not return anything, modify nums in-place instead.
 
 class Solution(object):
     def moveZeroes(self, nums):
-        count = 0
-        while 0 in nums:
-            nums.remove(0)  # Remove the first occurrence of 0
-            count += 1  # Increment the count
-        nums.extend([0] * count)
+        non_zero = 0  # Pointer for non-zero elements
+        # Move all non-zero elements to the front
+        for i in range(len(nums)):
+            if nums[i] != 0:
+                # swap
+                nums[i], nums[non_zero] = nums[non_zero], nums[i]
+                non_zero += 1
         
+
+# ALTERNATE APPROACH   
+
+# class Solution(object):
+#     def moveZeroes(self, nums):
+#         count = 0
+#         while 0 in nums:
+#             nums.remove(0)  # Remove the first occurrence of 0
+#             count += 1  # Increment the count
+#         nums.extend([0] * count)
