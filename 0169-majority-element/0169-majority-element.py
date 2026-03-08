a31fe02f2519dc64c2ba155c7a8ae0aa6f98b276
Mohammad Hassan Imran <i220813@nu.edu.pk>
2026-03-08 09:04:35 +0500

    Time: 23 ms (9.08%), Space: 13.6 MB (66.07%) - LeetHub

---
+++ 0169-majority-element/0169-majority-element.py
@@ -0,0 +1,9 @@
+from collections import Counter
+class Solution(object):
+    def majorityElement(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        return Counter(nums).most_common()[0][0]
+        
