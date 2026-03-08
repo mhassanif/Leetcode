0ccabecfb91d79fbb695c992d77c45be2b3d4e2c
Mohammad Hassan Imran <i220813@nu.edu.pk>
2026-03-08 09:51:42 +0500

    Time: 0 ms (100%), Space: 12.4 MB (99.35%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def isValid(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        pairs = {')':'(',']':'[','}':'{'}
+        stack=[]
+        for c in s:
+            if c in '([{':
+                stack.append(c)
+            else:
+                if not stack or stack[-1]!=pairs[c]:
+                    return False
+                stack.pop()
+        return len(stack)==0
