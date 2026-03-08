89ef38e04c5b9369574df8d80fe5a57a54f4dfe0
Mohammad Hassan Imran <i220813@nu.edu.pk>
2026-03-08 09:18:55 +0500

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0169-majority-element/0169-majority-element.py
+++ 0169-majority-element/0169-majority-element.py
@@ -5,15 +5,15 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        return Counter(nums).most_common(1)[0][0]
-        # freq = {}
-        # for x in nums:
-        #     freq[x] = freq.get(x,0)+1
+        # return Counter(nums).most_common(1)[0][0]
+        freq = {}
+        for x in nums:
+            freq[x] = freq.get(x,0)+1
         
-        # max_count = 0
-        # majority = nums[0]
-        # for num in freq:
-        #     if freq[num] > max_count:
-        #         max_count = freq[num]
-        #         majority = num
-        # return majority
+        max_count = 0
+        majority = nums[0]
+        for num in freq:
+            if freq[num] > max_count:
+                max_count = freq[num]
+                majority = num
+        return majority
