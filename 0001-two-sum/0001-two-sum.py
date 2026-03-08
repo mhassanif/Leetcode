cd50549511b757f9dae745ffa62ca9246a0297a6
Mohammad Hassan Imran <i220813@nu.edu.pk>
2026-03-08 08:08:40 +0500

    Time: 0 ms (100%), Space: 12.8 MB (98.32%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -5,17 +5,9 @@ class Solution(object):
         :type target: int
         :rtype: List[int]
         """
-        # Create a dictionary to store the value and its index
-        num_dict = {}
-        
-        # Iterate through the list of numbers
-        for i, num in enumerate(nums):
-            # Calculate the complement of the current number
-            complement = target - num
-            
-            # If the complement is already in the dictionary, return its index and the current index
-            if complement in num_dict:
-                return [num_dict[complement], i]
-            
-            # Otherwise, add the current number to the dictionary with its index
-            num_dict[num] = i
+        visited = {}
+        for i,num in enumerate(nums):
+            need = target - num
+            if need in visited:
+                return [visited[need],i]
+            visited[num]=i
