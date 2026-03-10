d2aea431fa6c19508ba19068d567b1f0663be6e1
Mohammad Hassan Imran <i220813@nu.edu.pk>
2026-03-10 13:09:27 +0500

    Time: 72 ms (49.75%), Space: 15.2 MB (99.34%) - LeetHub

--- 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.py
+++ 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.py
@@ -4,23 +4,14 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        slow=1 # keep track of where to write 
+        slow=1
         count=1
         for fast in range(1,len(nums)):
-            # count tracking
-            if nums[fast]!=nums[fast-1]:
-                # new number - reset count
+            if nums[fast-1]!=nums[fast]:
                 count=1
             else:
-                # old number - increment count
                 count+=1
-            
-            # write or not
             if count<=2:
-                # number came first or second time max
-                # only happens after count reset 
                 nums[slow]=nums[fast]
                 slow+=1
         return slow
-
-
