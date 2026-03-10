562de4f7a25c8a45cb1e1f88d4e1519e30478d59
Mohammad Hassan Imran <i220813@nu.edu.pk>
2026-03-10 12:14:52 +0500

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0027-remove-element/0027-remove-element.py
+++ 0027-remove-element/0027-remove-element.py
@@ -4,15 +4,18 @@ class Solution(object):
         :type nums: List[int]
         :type val: int
         :rtype: int
+        - non-val is copied and k moves
+        - if its val k doesn't move ie stays at position of val
+        - when i meets a non val it copies that to postion where val was
+        - hence val gets overwritten
+        - since k stopped eveytime it met a val
+        - it only moved forward only non val times
+        - hence directly return k
         """
-        idx=[]
-        # mark the positons
+        k = 0 # copy position
         for i in range(len(nums)):
             if nums[i]==val:
-                idx.append(i)
-        left_shift=0
-        # pop the postions considering shift
-        for i in range(len(idx)):
-            nums.pop(idx[i]-left_shift)
-            left_shift+=1
-        return len(nums)
+                # copy non-val back 
+                nums[k]=nums[i]
+                k+=1
+        return k 
