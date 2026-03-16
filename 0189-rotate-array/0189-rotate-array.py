48acc15c410017df4617455f60eb45085686c629
Mohammad Hassan Imran <i220813@nu.edu.pk>
2026-03-16 12:54:43 +0500

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.py
+++ 0189-rotate-array/0189-rotate-array.py
@@ -5,6 +5,5 @@ class Solution(object):
         :type k: int
         :rtype: None Do not return anything, modify nums in-place instead.
         """
-        k = k % len(nums)
-        nums[:] = nums[-k:] + nums[:-k]
+        nums[:] = nums[-k:] + nums[:-k] 
         
